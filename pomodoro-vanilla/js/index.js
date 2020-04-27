9aa8ca63ccfedf6500b76fb6cc4a9536c12e7081
Simon Moe <simmoe@gmail.com>
2020-04-27 19:26:48 +0200

    add pomodoro skeleton

---
+++ pomodoro-vanilla/js/index.js
@@ -0,0 +1,10 @@
+// fetch ui elements
+var timeTextObject = document.querySelector('.time')
+var unitTextObject = document.querySelector('.unit')
+var startButton    = document.querySelector('.button')
+
+
+
+
+
+
